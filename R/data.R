bd6cada61cd49d3219ce073eaaa58f8a5b4cae2d
Wolfgang <wolfgang.schadner@outlook.com>
2022-05-13 11:41:07 +0200

    adding basic functionality

---
+++ R/data.R
@@ -0,0 +1,5 @@
+#' Test sample for the option-implied correlation matrix of S&P 100 stock market index
+#'
+#' 
+"sp100"
+
--- R/irspg.R
+++ R/irspg.R
@@ -1,14 +1,4 @@
-
-
-# New algo 2021-05-04 -----------------------------------------------------
-
-# Note: 
-# the IR handles the equality constraint separately from the Omega constraint
-# Omega represents X^2 <= 1
-# C(X) defines the Lagrangian
-# -> for projection, use the one as in the un-market-constrained case
-
-irspg <- function(X0, fn=NULL, grad_fn=NULL, Ceq=NULL, grad_Ceq=NULL,
+irspg <- function(X0, objfn=NULL, objfn_g=NULL, projfn=NULL, eqfn=NULL, eqfn_g=NULL, nmls=FALSE,
                     cntrl=list(eps_opt=1e-2, eta_min=1e-15, eta_max=1e+15, 
                     eps_feas=1e-6, kmax=50, ktmax=5, gtol=1e-01, ftol=1e-01), ...){
   gtol <- cntrl$gtol
@@ -17,17 +7,15 @@ irspg <- function(X0, fn=NULL, grad_fn=NULL, Ceq=NULL, grad_Ceq=NULL,
   eta_min <- cntrl$eta_min
   eta_max <- cntrl$eta_max
   eps_feas <- cntrl$eps_feas
-  vtol <- eps_feas
+  eq_error <- eps_feas
   eps_opt <- cntrl$eps_opt
   kmax <- cntrl$kmax
   ktmax <- cntrl$ktmax
   
-  # fk <- dim(as.matrix(X0))[2]
-  
-  ## functions ##
+  # functions 
   etak_f <- function(){
     sk <- xk - xk_prev
-    uk <- c(grad_fn(xk)) - c(grad_fn(xk_prev))
+    uk <- c(objfn_g(xk)) - c(objfn_g(xk_prev))
     
     su <- c(t(sk) %*% uk)
     if(su > 0){
@@ -39,43 +27,37 @@ irspg <- function(X0, fn=NULL, grad_fn=NULL, Ceq=NULL, grad_Ceq=NULL,
   }
   
   restoration_fn <- function(xk){
-    yhat <- proj_f(xk)
+    yhat <- projfn(xk)
     yk_f <- function(lambda) lambda*xk + (1-lambda)*yhat
     l <- 0.1
     ii <- 1 - seq(0, 1, length.out=100)
     for(ll in ii){
       yk <- yk_f(ll)
-      if(abs(Ceq(yk)) <= rr*nC_xk){ 
+      if(abs(eqfn(yk)) <= rr*nC_xk){ 
         break
       }
     }
     return(yk)
   }
   
-  ## Initial Parameters ##
+  # Initial Parameters
   
-  xk <- proj_f(X0)
+  xk <- projfn(X0)
   th_prev <- 0.99 # in (0,1)
   rr <- 0.25 # in [0,1)
   beta <- n*10 # > 0
   
-  ## Iterations ##
+  # Iterations
   xk_prev <- xk - 0.1
   
-  nC_xk <- abs(Ceq(xk))
-  fn0 <- fn(xk)
+  nC_xk <- abs(eqfn(xk))
+  objfn0 <- objfn(xk)
   
   ct <- Sys.time()
   for(k in 0:kmax){
-    # print(paste("iteration:", k, "| Gradient:", round(norm(grad_fn(xk), "2"), 3), "| Constraint:", round(Ceq(xk), 6), "| Objective:", round(fn(xk), 3)))
-    # print(paste("iteration:", k, "    |dk|:", norm(dk, "2")))
     
-    gr_prev <- norm(grad_fn(xk), "2")
-    fn_prev <- fn(xk)
-    
-    # Step 1
-    # Note: for equality constraints this writes different than in the paper,
-    # see: https://www.ime.unicamp.br/~martinez/irexpanded.pdf
+    gr_prev <- norm(objfn_g(xk), "2")
+    objfn_prev <- objfn(xk)
     
     # Step 1: Restoration, yk
     yk <- restoration_fn(xk)
@@ -86,47 +68,49 @@ irspg <- function(X0, fn=NULL, grad_fn=NULL, Ceq=NULL, grad_Ceq=NULL,
     
     # Step 3-4: Non-Monotone Line Search 
     etak <- etak_f()
-    dk <- proj_f(yk - etak * grad_fn(yk)) - yk
     
-    # # non-monotone line search:
-    # for(kt in 1:ktmax){
-    #   z <- yk + dk
-    #   if(abs(Ceq(z)) <= rr*nC_xk){ #& norm(z - xk, "2") <= beta*nC_xk){
-    #     yk <- z
-    #     dk <- proj_f(yk - etak * grad_fn(yk)) - yk
-    #   } else {
-    #     break
-    #   }
-    # }
-    # 
-    nC_yk <- abs(Ceq(yk)) 
+    if(nmls){
+      # non-monotone line search:
+      for(kt in 1:ktmax){
+        z <- yk + dk
+        if(abs(eqfn(z)) <= rr*nC_xk){ #& norm(z - xk, "2") <= beta*nC_xk){
+          yk <- z
+          dk <- projfn(yk - etak * objfn_g(yk)) - yk
+        } else {
+          break
+        }
+      }
+    } else {
+      dk <- projfn(yk - etak * objfn_g(yk)) - yk  
+    }
+
+    nC_yk <- abs(eqfn(yk)) 
     
     # Step 5
     t <- 1
-    # t <- 10
-    gr_fyk <- grad_fn(yk)
+    gr_fyk <- objfn_g(yk)
     cdf <- crossprod(dk, gr_fyk)
-    while(fn(yk + t*dk) > (fn(yk) + 0.1 * t * cdf)){
+    while(objfn(yk + t*dk) > (objfn(yk) + 0.1 * t * cdf)){
       t <- 0.9 * t
     }
     tdec <- tki <- t
     zki <- yk + tki*dk
     
     # alternative stopping criteria
-    # if((abs(Ceq(zki)) <= eps_feas) & abs(gr_prev - sum(abs(grad_fn(zki)))) <= gtol){
-    if(abs(Ceq(zki)) <= eps_feas){
-      if(gr_prev -  norm(grad_fn(zki), "2") <= gtol | (fn_prev - fn(zki)) <= ftol){
+    # if((abs(eqfn(zki)) <= eps_feas) & abs(gr_prev - sum(abs(objfn_g(zki)))) <= gtol){
+    if(abs(eqfn(zki)) <= eps_feas){
+      if(gr_prev -  norm(objfn_g(zki), "2") <= gtol | (objfn_prev - objfn(zki)) <= ftol){
         xk <- zki
         ct <- Sys.time() - ct
-        return(list(xk=xk, iter_out=k+1, vtol=Ceq(xk), ctime=ct, fn=fn(xk), opti_at_first=(fn(xk > fn0))))
+        return(list(X=xk, iter_out=k+1, eq_error=eqfn(xk), ctime=ct, objfn=objfn(xk), opti_at_first=(objfn(xk > objfn0))))
       }
     }
     
-    # # Step 6
+    # Step 6
     th <- seq(0, thki_prev, length.out = 5)
-    dfz <- fn(xk) - fn(zki)
-    dcy <- nC_xk - norm(Ceq(yk), "2")
-    dcz <- nC_xk - norm(Ceq(zki), "2")
+    dfz <- objfn(xk) - objfn(zki)
+    dcy <- nC_xk - norm(eqfn(yk), "2")
+    dcz <- nC_xk - norm(eqfn(zki), "2")
     
     aa <- th*dfz + (1-th)*dcy
     aa <- aa[aa >= 0.5*dcy]
@@ -134,7 +118,7 @@ irspg <- function(X0, fn=NULL, grad_fn=NULL, Ceq=NULL, grad_Ceq=NULL,
     
     xk_prev <- xk
     xk <- zki
-    nC_xk <- abs(Ceq(xk))
+    nC_xk <- abs(eqfn(xk))
     thk <- thki
     th_prev <- c(th_prev, thk)
   }
